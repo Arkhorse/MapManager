d8e01291581687cba4c619b38738098a8db95135
Arkhorse <theillusivemman@gmail.com>
2023-05-26 16:48:21 -0300

    add desc, change reveal map to hotkey, add debug logging

--- Settings.cs
+++ Settings.cs
@@ -8,30 +8,47 @@ namespace MapManager
 
         [Section("Main")]
         [Name("Enable Map Arrow")]
-        public bool EnableArrow = false;
+        [Description("If enabled, will show where the player is on the map with an arrow")]
+        public bool EnableArrow             = false;
 
         [Name("Center on the player")]
-        public bool CenterOnPlayer = false;
+        [Description("If enabled, will center on where the player is on the map")]
+        public bool CenterOnPlayer          = false;
 
-        [Name("Always reveal the map")]
-        public bool RevealMap = false;
+        [Name("Reveal The map")]
+        [Description("Set this to be able to map the entire region")]
+        public KeyCode RevealMap            = KeyCode.None;
 
         [Section("Map Surveying")]
         [Name("Range Mulitplier")]
         [Description("Vanilla = 25")]
         [Slider(0.1f, 5f)]
-        public float MapSurveyMult = 1f;
+        public float MapSurveyMult          = 1f;
 
         [Name("Map Surveying Time Multiplier")]
-        [Description("")]
+        [Description("The amount of time it takes to survey")]
         [Slider(0.1f, 5f)]
-        public float MapSurveyMultTime = 1f;
+        public float MapSurveyMultTime      = 1f;
 
-        [Name("Polarods Discovered")]
-        [Description("Surveying vistas w/o polaroid will silently add it to inventory. Finding polaroids will not discover their locations.")]
-        public bool MapWithPolariods = false;
+        [Name("Polaroid's Discovered")]
+        [Description("Surveying vistas w/o polaroid's will silently add it to inventory. Finding polaroid's will not discover their locations")]
+        public bool MapWithPolariods =       false;
 
-        internal void OnLoad()
+#if DEBUG
+        protected override void OnConfirm()
+        {
+            MapManager.Log($"==============================================================================");
+            MapManager.Log($"EnableArrow:           {EnableArrow}       ");
+            MapManager.Log($"CenterOnPlayer:        {CenterOnPlayer}    ");
+            MapManager.Log($"RevealMap:             {RevealMap}         ");
+            MapManager.Log($"MapSurveyMult:         {MapSurveyMult}     ");
+            MapManager.Log($"MapSurveyMultTime:     {MapSurveyMultTime} ");
+            MapManager.Log($"MapWithPolariods:      {MapWithPolariods}  ");
+            MapManager.Log($"==============================================================================");
+        }
+#endif
+
+        internal static void OnLoad()
         {
             _settings.AddToModSettings("Map Manager");
             //SetFieldVisible(nameof(EnableArrow), true);
