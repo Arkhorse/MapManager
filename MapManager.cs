fc49aedf49d2f01d61cef836074113563cf019eb
Arkhorse <theillusivemman@gmail.com>
2023-05-26 16:14:39 -0300

    upd to use template style

--- MapManager.cs
+++ MapManager.cs
@@ -1,39 +1,6 @@
-﻿global using System.Reflection;
-global using Il2Cpp;
-global using HarmonyLib;
-global using MelonLoader;
-global using ModSettings;
-global using UnityEngine;
-
-namespace MapManager
+﻿namespace MapManager
 {
-    public static class BuildInfo
-    {
-        /// <summary>
-        /// Internal mod name
-        /// </summary>
-        internal const string ModID         = "MapManager";
-        /// <summary>
-        /// Human readable mod name
-        /// </summary>
-        internal const string ModName       = "Map Manager";
-        /// <summary>
-        /// Current Author (not including any credits)
-        /// </summary>
-        internal const string ModAuthor     = "The Illusion";
-        /// <summary>
-        /// List of all credited entities
-        /// </summary>
-        internal const string ModCredits    = "ttr";
-        /// <summary>
-        /// Current mod version
-        /// </summary>
-        internal const string ModVersion    = "1.1.0";
-        /// <summary>
-        /// Contains a description as to what the mod does
-        /// </summary>
-        internal const string ModDesc       = "Adds settings to better manage the map";
-    }
+
     internal class MapManager : MelonMod
     {
         public override void OnInitializeMelon()
@@ -50,11 +17,7 @@ namespace MapManager
         /// <param name="parameters"></param>
         internal static void Log(string message, params object[] parameters)
         {
-            MelonLogger.Msg($"[Map Manager]: {message}", parameters);
+            MelonLogger.Msg($"{message}", parameters);
         }
-        //internal static string GithubProfileURL(string url)
-        //{
-        //    return $"https://github.com/{url}";
-        //}
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using MelonLoader;
+﻿using BuildInfo = MapManager.BuildInfo;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MapManager")]
-[assembly: AssemblyDescription("Adds settings to better manage the map")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MapManager")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle(BuildInfo.Name)]
+[assembly: AssemblyDescription(BuildInfo.Description)]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyProduct(BuildInfo.Product)]
+[assembly: AssemblyCopyright(BuildInfo.Copyright)]
+[assembly: AssemblyTrademark(BuildInfo.Trademark)]
+[assembly: AssemblyCulture(BuildInfo.Culture)]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-
-// Important Melon info
-[assembly: MelonInfo(
-    type:       typeof(MapManager.MapManager),
-    name:       "MapManager",
-    version:    "1.1.1",
-    author:     "The Illusion")]
-
-[assembly: MelonGame("Hinterland", "TheLongDark")]
+[assembly: AssemblyVersion(BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: MelonInfo(typeof(MapManager.MapManager), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
 
 [assembly: MelonIncompatibleAssemblies("MapTweaks")]
-[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
