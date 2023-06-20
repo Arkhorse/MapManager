2e4b1cd72c7926f9d5eab594ee97e5226be80177
Arkhorse <theillusivemman@gmail.com>
2023-06-20 15:15:13 -0300

    upd ref

--- VisualStudio/Utilities/Logger.cs
+++ VisualStudio/Utilities/Logger.cs
@@ -2,10 +2,10 @@ namespace MapManager
 {
     public class Logger
     {
-        public static void Log(string message, params object[] parameters)              => Melon<TEMPLATE>.Logger.Msg($"{message}", parameters);
-        public static void LogWarning(string message, params object[] parameters)       => Melon<TEMPLATE>.Logger.Warning($"{message}", parameters);
-        public static void LogError(string message, params object[] parameters)         => Melon<TEMPLATE>.Logger.Error($"{message}", parameters);
-        public static void LogSeperator(params object[] parameters)                     => Melon<TEMPLATE>.Logger.Msg("==============================================================================", parameters);
-        public static void LogStarter()                                                 => Melon<TEMPLATE>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
+        public static void Log(string message, params object[] parameters)              => Melon<MapManager>.Logger.Msg($"{message}", parameters);
+        public static void LogWarning(string message, params object[] parameters)       => Melon<MapManager>.Logger.Warning($"{message}", parameters);
+        public static void LogError(string message, params object[] parameters)         => Melon<MapManager>.Logger.Error($"{message}", parameters);
+        public static void LogSeperator(params object[] parameters)                     => Melon<MapManager>.Logger.Msg("==============================================================================", parameters);
+        public static void LogStarter()                                                 => Melon<MapManager>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
     }
 }
