2d051998c5e3fd88a50097acf34791c0fe4d8740
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-30 20:48:04 -0300

    Create BaseAI_Start.cs

---
+++ VisualStudio/Patches/BaseAI_Start.cs
@@ -0,0 +1,11 @@
+﻿//namespace MapManager.Patches
+//{
+//    [HarmonyPatch(typeof(BaseAi), nameof(BaseAi.Start))]
+//    public class BaseAI_Start
+//    {
+//        public static void Postfix(BaseAi ai)
+//        {
+
+//        }
+//    }
+//}
