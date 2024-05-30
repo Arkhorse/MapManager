d827dc16045f8e6c25740c5f255acf6a1ff397d1
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-30 20:48:29 -0300

    add ability to map at any time

---
+++ VisualStudio/Patches/CharcoalItem_HasSurveyVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapManager.Patches
+{
+	[HarmonyPatch(typeof(CharcoalItem), nameof(CharcoalItem.HasSurveyVisibility), [ typeof(float) ])]
+	public class CharcoalItem_HasSurveyVisibility
+	{
+		public static bool Prefix(ref bool __result)
+		{
+			__result = Settings.Instance.UnlockSurvey;
+			return !Settings.Instance.UnlockSurvey;
+		}
+	}
+}
--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -2,71 +2,51 @@
 
 namespace MapManager
 {
-    internal class Settings : JsonModSettings
-    {
-        internal static Settings Instance { get; } = new();
+	internal class Settings : JsonModSettings
+	{
+		internal static Settings Instance { get; } = new();
 
-        [Section("Main")]
-        [Name("Enable Map Arrow")]
-        [Description("If enabled, will show where the player is on the map with an arrow")]
-        public bool EnableArrow                 = false;
+		[Section("Main")]
+		[Name("Enable Map Arrow")]
+		[Description("If enabled, will show where the player is on the map with an arrow")]
+		public bool EnableArrow                 = false;
 
-        [Name("Center on the player")]
-        [Description("If enabled, will center on where the player is on the map")]
-        public bool CenterOnPlayer              = false;
+		[Name("Center on the player")]
+		[Description("If enabled, will center on where the player is on the map")]
+		public bool CenterOnPlayer              = false;
 
-        [Name("BETA: Reveal The map")]
-        [Description("Set this to be able to map the entire region")]
-        public KeyCode RevealMap                = KeyCode.KeypadDivide;
+		[Name("BETA: Reveal The map")]
+		[Description("Set this to be able to map the entire region")]
+		public KeyCode RevealMap                = KeyCode.KeypadDivide;
 
-        [Section("Map Surveying")]
-        [Name("Range Mulitplier")]
-        //[Description("Vanilla = 25")]
-        [Slider(-0.99f, 5f, NumberFormat = "{0:F2}")]
-        public float MapSurveyMult              = 1f;
+		[Section("Map Surveying")]
+		[Name("Range Mulitplier")]
+		//[Description("Vanilla = 25")]
+		[Slider(-0.99f, 5f, NumberFormat = "{0:F2}")]
+		public float MapSurveyMult              = 1f;
 
-        [Name("Map Surveying Time Multiplier")]
-        [Description("The amount of time it takes to survey")]
-        [Slider(0.1f, 5f, NumberFormat = "{0:F2}")]
-        public float MapSurveyMultTime          = 1f;
+		[Name("Map Surveying Time Multiplier")]
+		[Description("The amount of time it takes to survey")]
+		[Slider(0.1f, 5f, NumberFormat = "{0:F2}")]
+		public float MapSurveyMultTime          = 1f;
 
-        [Name("Polaroid's Discovered")]
-        [Description("Surveying vistas w/o polaroid's will silently add the relevant Polaroid to your inventory. Finding Polaroid's will not reveal their locations.")]
-        public bool MapWithPolariods            = false;
+		[Name("Polaroid's Discovered")]
+		[Description("Surveying vistas w/o polaroid's will silently add the relevant Polaroid to your inventory. Finding Polaroid's will not reveal their locations.")]
+		public bool MapWithPolariods            = false;
 
-        [Name("Reveal Vista Locations")]
-        [Description("Will force reveal any vista locations within range")]
-        public bool RevealVista                 = false;
+		[Name("Reveal Vista Locations")]
+		[Description("Will force reveal any vista locations within range")]
+		public bool RevealVista                 = false;
 
-        protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
-        {
-            if ( field.Name == nameof(EnableArrow)
-                 || field.Name == nameof(CenterOnPlayer)
-                 || field.Name == nameof(RevealMap)
-                 || field.Name == nameof(MapSurveyMult)
-                 || field.Name == nameof(MapSurveyMultTime)
-                 || field.Name == nameof(MapWithPolariods)
-                 || field.Name == nameof(RevealVista) )
-            {
-                Instance.Refresh();
-            }
-            base.OnChange(field, oldValue, newValue);
-        }
+		[Name("Unlock Survey")]
+		[Description("Allow surveying at any time")]
+		public bool UnlockSurvey                = false;
 
-        private void Refresh()
-        {
-            SetFieldVisible(nameof(EnableArrow),        true);
-            SetFieldVisible(nameof(CenterOnPlayer),     true);
-            SetFieldVisible(nameof(RevealMap),          true);
-            SetFieldVisible(nameof(MapSurveyMult),      true);
-            SetFieldVisible(nameof(MapSurveyMultTime),  true);
-            SetFieldVisible(nameof(MapWithPolariods),   true);
-        }
 
-        internal static void OnLoad()
-        {
-            Instance.AddToModSettings($"{BuildInfo.GUIName}");
-            Instance.Refresh();
-        }
-    }
+		internal static void OnLoad()
+		{
+			Instance.AddToModSettings($"{BuildInfo.GUIName}");
+			Instance.RefreshGUI();
+		}
+	}
 }
