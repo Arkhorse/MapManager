7613ae501eea3f297900f200130a4770462ba8a6
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-06-06 16:13:57 -0300

    flipped logic and refined logging message

--- VisualStudio/Patches/VistaLocation_HasRequiredGearItem.cs
+++ VisualStudio/Patches/VistaLocation_HasRequiredGearItem.cs
@@ -5,15 +5,15 @@
     {
         private static void Postfix(VistaLocation __instance, ref bool __result)
         {
-            if (!__result)
+			if (Settings.Instance.MapWithPolariods && !__result)
+			{
+				Main.Logger.Log($"Add: {__instance.m_RequiredGearItem.GetDisplayNameWithoutConditionForInventoryInterfaces()}", FlaggedLoggingLevel.Debug);
+				GameManager.GetPlayerManagerComponent().AddItemToPlayerInventory(__instance.m_RequiredGearItem, true, true);
+				__result = true;
+			}
+			if (!__result)
             {
-                Main.Logger.Log("Result is false", FlaggedLoggingLevel.Debug);
-            }
-            if (Settings.Instance.MapWithPolariods && !__result)
-            {
-                Main.Logger.Log($"Add: {__instance.m_RequiredGearItem.GetDisplayNameWithoutConditionForInventoryInterfaces()}", FlaggedLoggingLevel.Debug);
-                GameManager.GetPlayerManagerComponent().AddItemToPlayerInventory(__instance.m_RequiredGearItem);
-                __result = true;
+                Main.Logger.Log($"{__instance.m_LocationName.Text()}: Result is false", FlaggedLoggingLevel.Verbose);
             }
         }
     }
